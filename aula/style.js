da2c3ed7fbfc0f1aba6075496684102b7f4a9e1e
Ricardo-P1nto <157704724+Ricardo-P1nto@users.noreply.github.com>
2024-11-21 14:07:31 +0000

    Add files via upload

--- aula/style.js
+++ aula/style.js
@@ -1,3 +1,25 @@
 function mudarCor() {
-    document.getElementById("titulo").style.color = "yellowgreen";
+  const titulo = document.getElementById("titulo");
+  if (titulo.style.color === "yellowgreen") {
+    titulo.style.color = "blue";
+  } else {
+    titulo.style.color = "yellowgreen";
   }
+}
+
+function mostrarPopUp() {
+  alert("Boas");
+}
+
+const imagens = [
+  "imagens/portugal.jpg",
+  "imagens/istockphoto-1221460597-612x612.jpg",
+  "imagens/fotos-de-portugal-60-1170x780.jpg"
+];
+
+let indiceAtual = 0;
+
+function mudarImagem() {
+  indiceAtual = (indiceAtual + 1) % imagens.length;
+  document.getElementById("imagem").src = imagens[indiceAtual];
+}
